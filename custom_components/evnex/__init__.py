0fe8e610324a2e36106dc8d3a7fb33ebb24ec40e
Brian Thorne <brian@thorne.link>
2022-10-28 00:16:20 +1300

    Charger enable/disable toggle and current limit slider

--- custom_components/evnex/__init__.py
+++ custom_components/evnex/__init__.py
@@ -227,8 +227,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
 
     # Setup components
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-    # await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    #hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
