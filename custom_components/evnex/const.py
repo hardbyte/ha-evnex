d21ebb415dcb0ce6dd57995187411bdd3c1f7e92
Brian Thorne <brian@thorne.link>
2024-05-04 20:21:05 +1200

    Set version to 0.6.0a1

--- custom_components/evnex/const.py
+++ custom_components/evnex/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 NAME = "evnex"
 DOMAIN = "evnex"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.5.2"
+VERSION = "0.6.0a1"
 ATTRIBUTION = "Data provided by https://evnex.io"
 ISSUE_URL = "https://github.com/hardbyte/ha-evnex/issues"
 
